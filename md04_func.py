47b710d6eafd55bd7d20fdc46e0065e7f2b5e0e6
Park <kr6u0102@festo.net>
2018-05-29 14:49:07 +0900

    error modified when numbers being converted

--- md04_func.py
+++ md04_func.py
@@ -1,5 +1,32 @@
 #https://stackoverflow.com/questions/11869910/pandas-filter-rows-of-dataframe-with-operator-chaining
 import pandas as pd
+import numpy as np
+import datetime
+
+category = {
+    'ShipNt' :  "Receipt",
+    'SchLne' :  "Receipt",    
+    'Stock'  :  "Receipt",
+    'PlOrd.'  :  "Receipt",
+    'CStock' : "Reqmt",
+    'Ordres' : "Reqmt",    
+    'Order'  :  "Reqmt",
+    'Deliv.' : "Reqmt",
+    'PrdOrd' : "", #normally assigned together with cstock. no need to count
+    'OrdRes' : "Reqmt",
+    'TrRes.' : "Reqmt",
+    'DepReq' : "Reqmt",
+    'RelOrd' : "Reqmt",
+    'Fr.del' : "Reqmt",
+    'SafeSt' : "Reqmt",
+    #'SafeSt' : "",
+    'StLcSt' : "",
+    'PurRqs' : "",
+    'IndReq' : ""
+}
+header=["Partnr","SO_Date","SO_MRP", "SO_NR", "SO_POS","SO_QTY", "PO_DATE","PO_MRP","PO_NR", "PO_POS","PO_QTY" ]
+total=[]
+
 
 def fileRead(filename) :
     items=[]
@@ -7,9 +34,9 @@ def fileRead(filename) :
     count=0
     toggle=False
 
-    with open(filename,"r") as file:
-    #ith open(filename,"r", encoding="UTF-8") as file:
-    #with open(filename,"r", encoding="ISO-8859-1") as file:
+    #with open(filename,"r") as file:
+    #with open(filename,"r", encoding="UTF-8") as file:
+    with open(filename,"r", encoding="ISO-8859-1") as file:
         document= file.readlines() 
     return document
 
@@ -42,25 +69,27 @@ def parsingDoc(document) :
             pos=cursor[4].strip().split("/")[1]
         
         except Exception as e:
-            docNum=0
+            docNum=cursor[4].strip()
             pos=0
         try  :           
 
             if int(cursor[7][-1] == "-") :                      
-                 qty=(-int(float((cursor[7][0:-1].strip()))))
-            else:
-                 qty=((int(float(cursor[7].strip()))))
+                 qty=(-int(float((cursor[7][0:-1].strip().replace(",","")))))
+            
+            else:                
+                qty=((int(float(cursor[7].strip().replace(",","")))))
         
         except Exception as e:
             qty=0
-            #print(cursor[7], cursor[8], "Exception occured")  
-            #print(e)
+            print(cursor[7], cursor[8], "Exception occured")  
+            print(e)
             
         try : 
+            print(cursor[8].strip().replace(",",""))
             if int(cursor[8][-1] == "-") :      
-                avail=(-int(float((cursor[8][0:-1].strip()))))                
+                avail=(-int(float((cursor[8][0:-1].strip().replace(",","")))))
             else:
-                avail=((int(float(cursor[8].strip()))))                
+                avail=((int(float(cursor[8].strip().replace(",","")))))                
                 
         except Exception as e:
             avail=0
@@ -82,3 +111,134 @@ def parsingDoc(document) :
 def mask(df, key, value):
     return df[df[key] == value]
 pd.DataFrame.mask = mask
+
+
+
+
+
+def kmatParser(df) : 
+    MappedResult=[]
+    idx=mask(df, "Type", "CStock").index
+    CStockReqmt=df.iloc[idx]
+    CStockReceipt=df.iloc[idx+1]
+
+    if len(CStockReqmt) != len(CStockReceipt) : 
+        print("Please check CStock as rows are not matched")
+    
+    for i,val in enumerate(CStockReceipt.iterrows()):
+        MappedResult.append (CStockReqmt.iloc[i].tolist()[0:6] + (CStockReceipt.iloc[i].tolist()[1:6]))
+    
+    return MappedResult, df.drop(index=idx+1).drop(index=idx)   
+
+
+
+def fehaParser(df) :
+    ReceiptQueue=[]
+    ReqmtQueue=[]
+    MappedResult=[]
+    flag=0
+    Receipt=[]
+    Reqmt=[]
+
+    
+    #KMAT PARSER
+    MappedResult,df = kmatParser(df)    
+    
+    
+    for index, line in df.iterrows() :    
+        print(line)
+        if line[6]==0  and line[2]== 'Stock' : continue
+        if line[2] == 'Stock' : 
+            line[5]=line[6]
+        if category[line[2]] == 'Receipt' :        
+            ReceiptQueue.append(line)
+        if category[line[2]] == 'Reqmt' :
+            ReqmtQueue.append(line)
+        else :
+            continue
+            
+    #If df only has kmat (CStock, then it return )
+    if len(df)<=1 :        
+        return MappedResult    
+    
+    # Else it goes through the below
+    else : 
+        try :   
+            print("DF")
+            print(df,"\n")
+            print("MappedResult")
+            print(MappedResult, "\n\n")
+            print("ReceiptQueue")
+            print(ReceiptQueue, "\n\n")
+            print("ReqmtQueue")
+            print(ReqmtQueue, "\n\n")
+            Receipt = ReceiptQueue.pop(0)
+            Reqmt = ReqmtQueue.pop(0)  
+
+            while True :                 
+                print (Receipt[5], Reqmt[5])
+                if Receipt[5] + Reqmt[5] == 0 :                    
+                    flag=0
+                    MappedResult.append([Reqmt[0], Reqmt[1], Reqmt[2], Reqmt[3], Reqmt[4], Reqmt[5],  Receipt[1], Receipt[2], Receipt[3], Receipt[4], Receipt[5]])    
+                    print("DF")
+                    print(df,"\n")
+                    print("ReceiptQueue")
+                    print(ReceiptQueue, "\n")
+                    print("ReqmtQueue")
+                    print(ReqmtQueue, "\n\n")
+                    
+                    if len(ReceiptQueue) +  len(ReqmtQueue) <=0 : 
+                        return MappedResult                        
+                    
+                    Receipt=[]; Receipt = ReceiptQueue.pop(0)
+                    Reqmt=[]; Reqmt = ReqmtQueue.pop(0)  
+
+                if Receipt[5] + Reqmt[5] > 0 :                           
+                    flag=1
+                    MappedResult.append([Reqmt[0], Reqmt[1], Reqmt[2], Reqmt[3], Reqmt[4], Reqmt[5],  Receipt[1], Receipt[2], Receipt[3], Receipt[4], -Reqmt[5]])    
+                    Receipt[5] +=Reqmt[5]            
+                    Reqmt = ReqmtQueue.pop(0)  
+
+
+                if Receipt[5] + Reqmt[5] < 0 :                                
+                    flag=-1
+                    MappedResult.append([Reqmt[0], Reqmt[1], Reqmt[2], Reqmt[3], Reqmt[4], -Receipt[5],  Receipt[1], Receipt[2], Receipt[3], Receipt[4], Receipt[5]])
+                    Reqmt[5] +=Receipt[5]
+                    Receipt = ReceiptQueue.pop(0)
+
+            #if no elment for popping.  
+        except Exception as e:   
+
+            if flag == -1:
+                MappedResult.append([Reqmt[0], Reqmt[1], Reqmt[2], Reqmt[3], Reqmt[4], Reqmt[5],'','' ,'' ,'' ,'' ]) 
+            elif flag == 1:
+                MappedResult.append([Receipt[0], '','','','','',  Receipt[1], Receipt[2], Receipt[3], Receipt[4], Receipt[5]])          
+
+            elif flag == 0:
+                print("flag 0")
+                print("Reqmt : ")
+                print(Reqmt,"\n")
+                
+                print("Receipt : ")
+                print(Receipt, "\n")
+                
+                
+                if len(Reqmt) == 0 and len(Receipt)==0 :                     
+                    print("both empty")
+                elif len(Reqmt) == 0 and len(Receipt)!=0: 
+                    #print("Reqmt empty")
+                    MappedResult.append([Receipt[0], '','','','','',  Receipt[1], Receipt[2], Receipt[3], Receipt[4], Receipt[5]])          
+                elif len(Reqmt) !=0 and len(Receipt) ==0 :                
+                    #print("Receipt empty")
+                    MappedResult.append([Reqmt[0], Reqmt[1], Reqmt[2], Reqmt[3], Reqmt[4], Reqmt[5],'','' ,'' ,'' ,'' ]) 
+
+            #print("ReqmtQueue :", len(ReqmtQueue), "ReceiptQueue :", len(ReceiptQueue))
+            for Reqmt in ReqmtQueue :
+                MappedResult.append([Reqmt[0], Reqmt[1], Reqmt[2], Reqmt[3], Reqmt[4], Reqmt[5],'','' ,'' ,'' ,'' ]) 
+
+            for Receipt in ReceiptQueue :
+                MappedResult.append([Receipt[0], '','','','','',  Receipt[1], Receipt[2], Receipt[3], Receipt[4], Receipt[5]])          
+            print ("!!!!!!!!!!!!!!!!!!!!!!!!")
+            print (MappedResult)
+            print ("!!!!!!!!!!!!!!!!!!!!!!!!")
+            return MappedResult
